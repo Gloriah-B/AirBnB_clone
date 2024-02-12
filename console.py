a64d23d92d43d7d1c5fc588524361e41a5a31632
Gloriah-B <Gloriah-B@github.com>
2024-02-12 07:31:42 -0800

    console.py

---
+++ console.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A simple Python script that prints "Hello, world!" to the console."""
+
+
+def main():
+    """Prints the greeting."""
+    print("Hello, world!")
+
+
+if __name__ == "__main__":
+    main()
